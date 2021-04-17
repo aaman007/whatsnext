c6756da63600487e151d2e150a694965aa42ddad
Amanur Rahman <aaman.sunny007@gmail.com>
2021-04-17 14:32:36 +0600

    Sidebar fixed width

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -69,7 +69,7 @@ const Container = styled.div`
   flex: 0.45;
   border-right: 1px solid whitesmoke;
   height: 100vh;
-  min-width: 300px;
+  min-width: 350px;
   max-width: 350px;
   overflow-y: scroll;
 
